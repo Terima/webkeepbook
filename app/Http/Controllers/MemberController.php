0080e91a9d2e07b0fdc51f263561e3dff2d30e68
Terima <tralop15@gmail.com>
2021-10-04 00:04:16 +0800

    Created table from members table to view

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
@@ -13,7 +14,8 @@ class MemberController extends Controller
      */
     public function index()
     {
-        //
+        $members = Member::all();
+        return view('backend.members', ['members' => $members]);
     }
 
     /**
--- resources/views/backend/members.blade.php
+++ resources/views/backend/members.blade.php
@@ -1,3 +1,33 @@
 @extends('layouts.backend.backend_dashboard')
 @section('title', 'Members')
 @section('dashboard_header', 'Member List')
+@section('content')
+<div class="row">
+    <div class="col-lg col-md-6 col-sm-6 mb-4">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Nomor Telepon</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($members as $key => $data)
+                <tr>
+                    <td>{{$data->id}}</td>
+                    <td>{{$data->name}}</td>
+                    <td>{{$data->address}}</td>
+                    <td>{{$data->email}}</td>
+                    <td>{{$data->password}}</td>
+                    <td>{{$data->phone_number}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
